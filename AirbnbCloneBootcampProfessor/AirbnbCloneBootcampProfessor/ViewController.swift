1e790499ac40e82496e5e572c87be199b823d335
Caio Fabrini <caiofabrini@outlook.com>
2024-08-05 22:04:26 -0300

    adicionando projeto configurado para viewCode

---
+++ AirbnbCloneBootcampProfessor/AirbnbCloneBootcampProfessor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirbnbCloneBootcampProfessor
+//
+//  Created by Caio Fabrini on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
