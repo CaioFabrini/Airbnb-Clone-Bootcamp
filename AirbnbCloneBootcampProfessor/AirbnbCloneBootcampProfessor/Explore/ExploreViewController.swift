6a058653992d0e0c946cc720859c7fc1babdc3f4
Caio Fabrini <caiofabrini@outlook.com>
2024-09-18 22:49:19 -0300

    criando mock para places

--- AirbnbCloneBootcampProfessor/AirbnbCloneBootcampProfessor/Explore/ExploreViewController.swift
+++ AirbnbCloneBootcampProfessor/AirbnbCloneBootcampProfessor/Explore/ExploreViewController.swift
@@ -21,19 +21,18 @@ class ExploreViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     viewModel.delegate = self
-//    viewModel.fetchCategoryListMock()
-    viewModel.fetchCategoryList()
-    screen?.configTableViewProtocols(delegate: self, dataSource: self)
+    viewModel.fetchAllMockRequest()
   }
 }
 
 extension ExploreViewController: ExploreViewModelProtocol {
   func success() {
     screen?.configCollectionViewProtocols(delegate: self, dataSource: self)
+    screen?.configTableViewProtocols(delegate: self, dataSource: self)
   }
   
   func failure(errorMessage: String) {
-
+    print(errorMessage)
   }
 }
 
